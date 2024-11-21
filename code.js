6bc23b52ceedc2b3dbd758d61f7011903ba54b53
Sebastian Hösl <sebastian.hoesl@outlook.de>
2024-11-21 22:12:33 +0100

    Refactoring: clean code

--- code.js
+++ code.js
@@ -11,77 +11,75 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 const filteredDevResourcesAsync = (sceneNode) => __awaiter(void 0, void 0, void 0, function* () {
     // Matching URLs from codepen.io, jsfiddle.net and lottie.host
     const urlRegex = new RegExp(/https?:\/\/(.+?\.)?((codepen\.io|jsfiddle\.net|lottie\.host|pastebin\.com)(\/[A-Za-z0-9\-._~:/?#[\]@!$&'()*+,;=]*)?)/);
-    const devResources = (yield sceneNode.getDevResourcesAsync({ includeChildren: true })).filter(x => x.url.match(urlRegex));
+    const devResources = (yield sceneNode.getDevResourcesAsync({ includeChildren: true })).filter((x) => x.url.match(urlRegex));
     return devResources;
 });
 const resolveResourceType = (url) => {
-    if (url.includes('//codepen.io/')) {
-        return 'codepen';
+    if (url.includes("//codepen.io/")) {
+        return "codepen";
     }
-    else if (url.includes('//jsfiddle.net/')) {
-        return 'jsfiddle';
+    else if (url.includes("//jsfiddle.net/")) {
+        return "jsfiddle";
     }
-    else if (url.includes('//lottie.host/')) {
-        return 'lottie';
+    else if (url.includes("//lottie.host/")) {
+        return "lottie";
     }
-    else if (url.includes('//pastebin.com/')) {
-        return 'pastebin';
+    else if (url.includes("//pastebin.com/")) {
+        return "pastebin";
     }
     else {
-        return 'unknown';
+        return "unknown";
     }
 };
 const getEmbedUrlAsync = (selectionIndex) => __awaiter(void 0, void 0, void 0, function* () {
     const unresolvedPromises = figma.currentPage.selection.map(filteredDevResourcesAsync);
     const results = yield Promise.all(unresolvedPromises);
-    const devResources = results.flatMap(x => x.flatMap(y => { return Object.assign(Object.assign({}, y), { type: resolveResourceType(y.url) }); }));
-    if (figma.editorType === "dev") {
-        switch (figma.mode) {
-            default: {
-                if (devResources && devResources.length > 0) {
-                    if (typeof selectionIndex !== "undefined") {
-                        const url = devResources[selectionIndex].url;
-                        const type = devResources[selectionIndex].type;
-                        let urlSplit;
-                        switch (type) {
-                            case 'codepen':
-                                urlSplit = url.substring(url.lastIndexOf('codepen.io/') + 11).split('/');
-                                figma.showUI(__uiFiles__.codepen.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-                                figma.ui.postMessage({ slugHash: urlSplit[2], user: urlSplit[1] });
-                                break;
-                            case 'jsfiddle':
-                                figma.showUI(__uiFiles__.jsfiddle.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-                                figma.ui.postMessage({ url });
-                                break;
-                            case 'lottie':
-                                figma.showUI(__uiFiles__.lottie.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-                                figma.ui.postMessage({ src: url });
-                                break;
-                            case 'pastebin':
-                                figma.showUI(__uiFiles__.pastebin.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-                                figma.ui.postMessage({ url });
-                                break;
-                        }
-                    }
-                    else {
-                        figma.showUI(__uiFiles__.navigation.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-                    }
-                    figma.ui.postMessage({ devResources });
-                }
-                else {
-                    figma.showUI(__uiFiles__.empty.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-                }
+    const devResources = results.flatMap((x) => x.flatMap((y) => {
+        return Object.assign({}, y);
+    }));
+    if (figma.editorType === "dev" && devResources && devResources.length > 0) {
+        if (typeof selectionIndex !== "undefined") {
+            const url = devResources[selectionIndex].url;
+            if (url.includes("//codepen.io/")) {
+                const urlSplit = url
+                    .substring(url.lastIndexOf("codepen.io/") + 11)
+                    .split("/");
+                figma.showUI(__uiFiles__.codepen.replace("{{svgIcons}}", __uiFiles__.icons), { themeColors: true });
+                figma.ui.postMessage({ slugHash: urlSplit[2], user: urlSplit[1] });
+            }
+            else if (url.includes("//jsfiddle.net/")) {
+                figma.showUI(__uiFiles__.jsfiddle.replace("{{svgIcons}}", __uiFiles__.icons), { themeColors: true });
+                figma.ui.postMessage({ url });
+            }
+            else if (url.includes("//lottie.host/")) {
+                figma.showUI(__uiFiles__.lottie.replace("{{svgIcons}}", __uiFiles__.icons), { themeColors: true });
+                figma.ui.postMessage({ src: url });
+            }
+            else if (url.includes("//pastebin.com/")) {
+                figma.showUI(__uiFiles__.pastebin.replace("{{svgIcons}}", __uiFiles__.icons), { themeColors: true });
+                figma.ui.postMessage({ url });
             }
         }
+        else {
+            figma.showUI(__uiFiles__.navigation.replace("{{svgIcons}}", __uiFiles__.icons), { themeColors: true });
+        }
+        figma.ui.postMessage({ devResources });
+    }
+    else {
+        figma.showUI(__uiFiles__.empty.replace("{{svgIcons}}", __uiFiles__.icons), {
+            themeColors: true,
+        });
     }
 });
 figma.ui.onmessage = (message) => __awaiter(void 0, void 0, void 0, function* () {
-    if (message.type === 'selection_change') {
+    if (message.type === "selection_change") {
         yield getEmbedUrlAsync(message.selectionIndex);
     }
-    else if (message.type === 'home') {
+    else if (message.type === "home") {
         yield getEmbedUrlAsync();
     }
 });
-figma.on("selectionchange", () => { getEmbedUrlAsync(); });
+figma.on("selectionchange", () => {
+    getEmbedUrlAsync();
+});
 getEmbedUrlAsync();
--- code.ts
+++ code.ts
@@ -1,76 +1,95 @@
 const filteredDevResourcesAsync = async (sceneNode: SceneNode) => {
   // Matching URLs from codepen.io, jsfiddle.net and lottie.host
-  const urlRegex: RegExp = new RegExp(/https?:\/\/(.+?\.)?((codepen\.io|jsfiddle\.net|lottie\.host|pastebin\.com)(\/[A-Za-z0-9\-._~:/?#[\]@!$&'()*+,;=]*)?)/);
-  const devResources = (await sceneNode.getDevResourcesAsync({ includeChildren: true })).filter(x => x.url.match(urlRegex));
+  const urlRegex: RegExp = new RegExp(
+    /https?:\/\/(.+?\.)?((codepen\.io|jsfiddle\.net|lottie\.host|pastebin\.com)(\/[A-Za-z0-9\-._~:/?#[\]@!$&'()*+,;=]*)?)/
+  );
+  const devResources = (
+    await sceneNode.getDevResourcesAsync({ includeChildren: true })
+  ).filter((x) => x.url.match(urlRegex));
   return devResources;
 };
 
 const resolveResourceType = (url: string) => {
-  if (url.includes('//codepen.io/')) {
-    return 'codepen';
-  } else if (url.includes('//jsfiddle.net/')) {
-    return 'jsfiddle';
-  } else if (url.includes('//lottie.host/')) {
-    return 'lottie';
-  } else if (url.includes('//pastebin.com/')) {
-    return 'pastebin';
+  if (url.includes("//codepen.io/")) {
+    return "codepen";
+  } else if (url.includes("//jsfiddle.net/")) {
+    return "jsfiddle";
+  } else if (url.includes("//lottie.host/")) {
+    return "lottie";
+  } else if (url.includes("//pastebin.com/")) {
+    return "pastebin";
   } else {
-    return 'unknown';
+    return "unknown";
   }
-}
+};
 
 const getEmbedUrlAsync = async (selectionIndex?: number) => {
-  const unresolvedPromises = figma.currentPage.selection.map(filteredDevResourcesAsync);
+  const unresolvedPromises = figma.currentPage.selection.map(
+    filteredDevResourcesAsync
+  );
   const results = await Promise.all(unresolvedPromises);
-  const devResources = results.flatMap(x => x.flatMap(y => { return { ...y, type: resolveResourceType(y.url) } }));
+  const devResources = results.flatMap((x) =>
+    x.flatMap((y) => {
+      return { ...y };
+    })
+  );
 
-  if (figma.editorType === "dev") {
-    switch (figma.mode) {
-      default: {
-        if (devResources && devResources.length > 0) {
-          if (typeof selectionIndex !== "undefined") {
-            const url = devResources[selectionIndex].url;
-            const type = devResources[selectionIndex].type;
-            let urlSplit;
-            switch (type) {
-              case 'codepen':
-                urlSplit = url.substring(url.lastIndexOf('codepen.io/') + 11).split('/');
-                figma.showUI(__uiFiles__.codepen.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-                figma.ui.postMessage({ slugHash: urlSplit[2], user: urlSplit[1] });
-                break;
-              case 'jsfiddle':
-                figma.showUI(__uiFiles__.jsfiddle.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-                figma.ui.postMessage({ url });
-                break;
-              case 'lottie':
-                figma.showUI(__uiFiles__.lottie.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-                figma.ui.postMessage({ src: url });
-                break;
-              case 'pastebin':
-                figma.showUI(__uiFiles__.pastebin.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-                figma.ui.postMessage({ url });
-                break;
-            }
-          } else {
-            figma.showUI(__uiFiles__.navigation.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-          }
-          figma.ui.postMessage({ devResources });
-        } else {
-          figma.showUI(__uiFiles__.empty.replace('{{svgIcons}}', __uiFiles__.icons), { themeColors: true });
-        }
+  if (figma.editorType === "dev" && devResources && devResources.length > 0) {
+    if (typeof selectionIndex !== "undefined") {
+      const url = devResources[selectionIndex].url;
+
+      if (url.includes("//codepen.io/")) {
+        const urlSplit = url
+          .substring(url.lastIndexOf("codepen.io/") + 11)
+          .split("/");
+        figma.showUI(
+          __uiFiles__.codepen.replace("{{svgIcons}}", __uiFiles__.icons),
+          { themeColors: true }
+        );
+        figma.ui.postMessage({ slugHash: urlSplit[2], user: urlSplit[1] });
+      } else if (url.includes("//jsfiddle.net/")) {
+        figma.showUI(
+          __uiFiles__.jsfiddle.replace("{{svgIcons}}", __uiFiles__.icons),
+          { themeColors: true }
+        );
+        figma.ui.postMessage({ url });
+      } else if (url.includes("//lottie.host/")) {
+        figma.showUI(
+          __uiFiles__.lottie.replace("{{svgIcons}}", __uiFiles__.icons),
+          { themeColors: true }
+        );
+        figma.ui.postMessage({ src: url });
+      } else if (url.includes("//pastebin.com/")) {
+        figma.showUI(
+          __uiFiles__.pastebin.replace("{{svgIcons}}", __uiFiles__.icons),
+          { themeColors: true }
+        );
+        figma.ui.postMessage({ url });
       }
+    } else {
+      figma.showUI(
+        __uiFiles__.navigation.replace("{{svgIcons}}", __uiFiles__.icons),
+        { themeColors: true }
+      );
     }
+    figma.ui.postMessage({ devResources });
+  } else {
+    figma.showUI(__uiFiles__.empty.replace("{{svgIcons}}", __uiFiles__.icons), {
+      themeColors: true,
+    });
   }
 };
 
 figma.ui.onmessage = async (message) => {
-  if (message.type === 'selection_change') {
+  if (message.type === "selection_change") {
     await getEmbedUrlAsync(message.selectionIndex);
-  } else if (message.type === 'home') {
+  } else if (message.type === "home") {
     await getEmbedUrlAsync();
   }
-}
+};
 
-figma.on("selectionchange", () => { getEmbedUrlAsync(); })
+figma.on("selectionchange", () => {
+  getEmbedUrlAsync();
+});
 
 getEmbedUrlAsync();
